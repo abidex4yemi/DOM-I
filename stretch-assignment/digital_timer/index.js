b5900f4c1a9d99990a524a6cda94360c244f9814
abidex4yemi <abidex4yemi@gmail.com>
2019-05-13 18:16:34 +0100

    complete stretch goal 1

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -6,13 +6,44 @@
 // Should not count past 10 seconds
 
 // Update the count down every 10ms second
-const counter = function() {
-	let counter = 0;
-	counter++;
+const msTens = document.querySelector('#msTens');
+msTens.textContent = 0;
 
-	return () => {
-		return counter++;
-	};
+const timer = () => {
+	let time = 0;
+	let hundredSeconds = 0;
+	let secondsOne = 0;
+	let secondTen = 0;
+
+	const interval = setInterval(() => {
+		time += 10;
+
+		if (time === 100) {
+			hundredSeconds++;
+			time = 0;
+		}
+
+		if (hundredSeconds === 10) {
+			secondsOne++;
+			hundredSeconds = 0;
+		}
+
+		if (secondsOne === 10) {
+			secondTen++;
+			secondsOne = 0;
+		}
+
+		if (secondTen) {
+			clearInterval(interval);
+			document.querySelectorAll('.digit').forEach(element => {
+				element.style.color = 'red';
+			});
+		}
+
+		document.querySelector('#msHundreds').textContent = hundredSeconds;
+		document.querySelector('#secondOnes').textContent = secondsOne;
+		document.querySelector('#secondTens').textContent = secondTen;
+	}, 10);
 };
 
-console.log(setInterval(counter, 10));
+timer();
